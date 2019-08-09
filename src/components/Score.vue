9acb6ca98d6bf45079313e41b644c2f533bf6b89
Elin Sexton <elin.sexton@gmail.com>
2019-08-09 19:44:39 +0200

    Score modal box works

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="modalBox" v-if="this.getRounds == 15">
-    <div class="modalContent">
+    <div class="modal-box" v-if="this.getRounds == 15">
+    <div class="modal-content">
         <div class="player-score">
-            <h1>Bravo! Du har klarat omgången!</h1>
-            <h2>Det här är dina poäng: {{score}}</h2>
+            <h1>Bravo!</h1>
+            <h1>Du har klarat omgången!</h1>
+            <h2>Dina poäng: {{score}}</h2>
         </div>
         <div>
+            <h2>Vill du spela igen?</h2>
             <button class="btn playAgain" @click="onBtnClick()">Spela Igen!</button>
         </div>
     </div>
@@ -24,7 +26,9 @@ export default {
         },
         replay: function() {
             this.$store.dispatch('replay');
-        }
+        },
+
+        
 
     },
     computed: {...mapGetters([
@@ -32,7 +36,7 @@ export default {
     'getPTwo',
     ]),
 
-    score: function() {
+        score: function() {
             return this.getPTwo[9].value;
         }
     }
@@ -42,24 +46,71 @@ export default {
 
 <style>
 
-/* Button*/
-.btn {
-    background-color: indigo;
-    border-radius: 5px;
+/* The Modal (background) */
+.modal-box {
+  height: 100%;
+  position: fixed;
+  top: 0;
+  margin-left: 5%;
+  border: 1px solid black;
+  background-color: gold;
 }
+/* Modal content */
+.modal-content {
+  text-align: center;
+  background-size: contain;
+  padding: 20px 30px;
+  color: black;
+  font-size: 18px;
+  margin: 5% auto;
+  width: 60%;
+  height: 75vh;
+}
+h1{
+  margin: 0px;
+}
+
+.playAgain{
+  border: 3px solid black;
+  text-align: center;
+  display: inline-block;
+  font-size: 18px;
+  box-shadow: 0 9px rgba(0,0,0,0.8);
+  margin: 10px;
+}
+.playAgain:hover{
+  background-color: #4da0d1;
+  cursor: pointer;
+}
+.playAgain:active{
+  background-color: #4da0d1;
+  box-shadow: 0 5px rgba(0,0,0,0.8);
+  transform: translateY(4px);
+}
+
 
 /*Desktop*/
 @media screen and (min-width: 501px){
-  .modalBox{
-    width: 200px;
+  .modal-box{
+    width: 90%;
   }
 }
 /*Mobile*/
 @media screen and (max-width: 500px) {
-  .modalBox{
-    width: 200px;
-    height: 400px;
+  .modal-box{
+    width: 90%;
+    height: 70%;
+    
   }
+  .playAgain:hover{
+  background-color: #4da0d1;
+  cursor: pointer;
+}
+.playAgain:active{
+  background-color: #4da0d1;
+  box-shadow: 0 5px rgba(0,0,0,0.8);
+  transform: translateY(4px);
+}
 }
 
 </style>
